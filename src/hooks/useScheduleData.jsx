923604ffa139a8067f219d5f19c6e3a0319face7
Utkarsh09102004 <utkarshagarwal204@gmail.com>
2024-09-03 14:23:56 +0530

    changing to old subgroup

--- src/hooks/useScheduleData.jsx
+++ src/hooks/useScheduleData.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import result from '@/assets/result(parsedOnly).json';
+import result from '@/assets/results.json';
 
 const useScheduleData = () => {
   const { sub } = useParams();
